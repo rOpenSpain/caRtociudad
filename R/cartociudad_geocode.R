d7f85b9996e45c9767675ff9720790a896367e55
Carlos Vergara-Hernández <vergara_car@gva.es>
2017-08-01 13:56:17 +0200

    Multiple queries in the same call

--- R/cartociudad_geocode.R
+++ R/cartociudad_geocode.R
@@ -18,7 +18,7 @@
 #' @param output_format Character string. Output format of the query: "JSON" or
 #'   "GeoJSON".
 #'
-#' @return A data frame consisting of a single row per guess. See the reference
+#' @return A data frame consisting of a single row per query. See the reference
 #'   below for an explanation of the data frame columns.
 #'
 #' @author Carlos J. Gil Bellosta
@@ -27,23 +27,54 @@
 #' \url{http://www.cartociudad.es/recursos/Documentacion_tecnica/CARTOCIUDAD_ServiciosWeb.pdf}
 #'
 #' @examples
-#' # using full address
-#' my.address <- cartociudad_geocode(full_address = "plaza de cascorro 11, 28005 madrid")
+#' # Query a single address
+#' address <- "plaza de cascorro 11, 28005 madrid"
+#' my.address <- cartociudad_geocode(full_address = address)
+#' print(my.address)
+#'
+#' # Query multiple addresses
+#' address <- c(address, "plaza del ayunamiento 1, valencia")
+#' my.address <- cartociudad_geocode(full_address = address)
 #' print(my.address)
 #'
 #' @export
 #'
 cartociudad_geocode <- function(full_address, output_format = "JSON") {
-    api.args <- list(q = full_address, outputformat = output_format)
+  names_res <- c("id", "province", "muni", "type", "address", "postalCode",
+                 "poblacion", "geom", "tip_via", "lat", "lng",
+                 "portalNumber", "stateMsg", "state", "countryCode")
+  results   <- as.data.frame(
+    matrix(
+      ncol = length(names_res),
+      nrow = length(full_address)
+    )
+  )
+  colnames(results) <- names_res
+  res_list          <- list()
+
+  for (i in seq_along(full_address)) {
+    api.args <- list(q = full_address[i], outputformat = output_format)
     ua  <- get_cartociudad_user_agent()
     res <- httr::GET("http://www.cartociudad.es/geocoder/api/geocoder/findJsonp",
                      query = api.args, ua)
-    httr::stop_for_status(res)
 
-    res <- jsonp_to_json(httr::content(res, as = "text", encoding = "UTF8"))
-    res <- jsonlite::fromJSON(res)
-    res <- as.data.frame(t(unlist(res)), stringsAsFactors = FALSE)
-    res[, c(grep("lat", names(res)), grep("lng", names(res)))] <-
-      apply(res[, c(grep("lat", names(res)), grep("lng", names(res)))], 2, as.numeric)
-    return(res)
+    if (httr::http_error(res)) {
+      warning("Error in query ", i, ": ", httr::http_status(res)$message)
+      results[i, "address"] <- full_address[i]
+      results[i, "state"]   <- 0
+    } else {
+      res <- jsonp_to_json(httr::content(res, as = "text", encoding = "UTF8"))
+      res <- jsonlite::fromJSON(res)
+      res_list[[i]] <- as.data.frame(t(unlist(res)), stringsAsFactors = FALSE)
+    }
+  }
+
+  if (length(res_list) == 1) {
+    results <- res_list[[1]]
+  } else {
+    results <- do.call(rbind_lists_df, res_list)
+  }
+
+  results[, c("lat", "lng")] <- apply(results[, c("lat", "lng")], 2, as.numeric)
+  return(results)
 }
--- R/cartociudad_reverse_geocode.R
+++ R/cartociudad_reverse_geocode.R
@@ -16,15 +16,8 @@
 #' @param longitude Point longitude in geographical coordinates (e.g.,
 #'   -3.7227241)
 #'
-#' @return A list with the following items:
-#' \item{tipo}{type of location.}
-#' \item{tipo.via}{road type.}
-#' \item{nombre.via}{road name.}
-#' \item{num.via}{road number.}
-#' \item{num.via.id}{internal id of this address in cartociudad database.}
-#' \item{municipio}{town.}
-#' \item{provincia}{province.}
-#' \item{cod.postal}{zip code.}
+#' @return A data frame consisting of a single row per query. See the reference
+#'   below for an explanation of the data frame columns.
 #'
 #' @author Luz Frias
 #'
@@ -32,34 +25,50 @@
 #' \url{http://www.cartociudad.es/recursos/Documentacion_tecnica/CARTOCIUDAD_ServiciosWeb.pdf}
 #'
 #' @examples
+#' # Query one point
 #' cartociudad_reverse_geocode(40.473219, -3.7227241)
+#'
+#' # Query multiple points
+#' cartociudad_reverse_geocode(c(40.473219, 39.46979), c(-3.7227241, -0.376963))
 #'
 #' @export
 #'
 cartociudad_reverse_geocode <- function(latitude, longitude) {
-  
-  query.parms <- list(
-    lat = latitude,
-    lon = longitude
+
+  stopifnot(length(latitude) == length(longitude))
+
+  names_res <- c("type", "tip_via", "address", "portalNumber", "id",
+                 "muni", "province", "postalCode", "lat", "lng")
+  results   <- as.data.frame(
+    matrix(
+      ncol = length(names_res),
+      nrow = length(latitude)
+    )
   )
-  
+  colnames(results) <- names_res
+  res_list          <- list()
+
   url <- "http://www.cartociudad.es/services/api/geocoder/reverseGeocode"
   ua <- get_cartociudad_user_agent()
-  
-  
-  res <- httr::GET(url, query = query.parms, ua)
-  httr::stop_for_status(res)
-  info <- httr::content(res)
-  # Parse the response
-  res <- list(
-    tipo       = info$type,
-    tipo.via   = info$tip_via,
-    nombre.via = info$address,
-    num.via    = info$portalNumber,
-    num.via.id = info$id,
-    municipio  = info$muni,
-    provincia  = info$province,
-    cod.postal = info$postalCode
-  )
-  return(res)
+
+  for (i in seq_along(latitude)) {
+    query.parms <- list(lat = latitude[i], lon = longitude[i])
+    res         <- httr::GET(url, query = query.parms, ua)
+
+    if (httr::http_error(res)) {
+      warning("Error in query ", i, ": ", httr::http_status(res)$message)
+      results[i, c("lat", "lng")] <- c(latitude[i], longitude[i])
+    } else {
+      info          <- httr::content(res)
+      res_list[[i]] <- as.data.frame(t(unlist(info)), stringsAsFactors = FALSE)[, names_res]
+    }
+  }
+
+  if (length(res_list) == 1) {
+    results <- res_list[[1]]
+  } else {
+    results <- do.call(rbind_lists_df, res_list)
+  }
+
+  return(results)
 }
