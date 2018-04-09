3c4e4575228dadc1d8858d2b5270e8f5f82d803c
Luz Frias <luzfrias@gmail.com>
2018-04-09 21:13:53 +0200

    Change IGN WMS service in get_cartociudadmap function

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 ##########################################################################
-# Luz Frias, 2016-06-03
+# Luz Frias, Carlos Gil Bellosta 2016-06-03
 # Common util functions
 ##########################################################################
 
@@ -13,4 +13,21 @@ jsonp_to_json <- function(text) {
   text <- gsub("^\\w+\\(", "", text)
   text <- gsub("\\)$", "", text)
   return(text)
+}
+
+overlay_wms_map <- function(map, url, query) {
+  ua <- get_cartociudad_user_agent()
+  res <- httr::GET(url, query = query, ua)
+  httr::warn_for_status(res) # data may not be available at a given map resolution
+  map <- tryCatch({
+    cs.layer <- httr::content(res, as = "parsed", type = "image/png")
+    # png overlay taking transparency into account
+    # note that cs layer has transparency 0/1
+    
+    mask <- array(cs.layer[, , 4], dim(cs.layer))
+    (1 - mask) * map + mask * cs.layer
+  },
+  error = function(x) return(map),  # we have already warned
+  warning = function(x) return(map) # ditto
+  )
 }
