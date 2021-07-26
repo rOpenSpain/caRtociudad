5dc6da06df4ab33f7a5b3414410030ae133e1718
dieghernan <diego.hernangomezherrero@gmail.com>
2021-07-26 13:41:35 +0200

    Update to testthat v3
    Also update actions and adds coverage

--- tests/testthat/test-caRtociudad.R
+++ tests/testthat/test-caRtociudad.R
@@ -1,46 +1,44 @@
-context("requests")
-
 test_that("cartociudad_geocode returns the location of a full address", {
   result <- cartociudad_geocode("plaza de cascorro 11, 28005 madrid")
 
-  expect_that(nrow(result) > 0, is_true())
+  expect_true(nrow(result) > 0)
 })
 
 test_that("get_cartociudad_map returns a map for a valid location", {
   map <- cartociudad_get_map(c(40.41137, -3.707168), 1)
 
-  expect_that(map, is_a("raster"))
-  expect_that(map, is_a("ggmap"))
+  expect_s3_class(map, "raster")
+  expect_s3_class(map, "ggmap")
 })
 
 test_that("get_cartociudad_location_info returns info for a valid location", {
   result <- cartociudad_get_location_info(40.473219, -3.7227241)
 
-  expect_that(!is.null(result$seccion),           is_true())
-  expect_that(!is.null(result$distrito),          is_true())
-  expect_that(!is.null(result$provincia),         is_true())
-  expect_that(!is.null(result$municipio),         is_true())
-  expect_that(!is.null(result$ref.catastral),     is_true())
-  expect_that(!is.null(result$url.ref.catastral), is_true())
-  expect_that(!is.null(result$tipo),              is_true())
-  expect_that(!is.null(result$tipo.via),          is_true())
-  expect_that(!is.null(result$nombre.via),        is_true())
-  expect_that(!is.null(result$num.via),           is_true())
-  expect_that(!is.null(result$num.via.id),        is_true())
-  expect_that(!is.null(result$cod.postal),        is_true())
+  expect_true(!is.null(result$seccion))
+  expect_true(!is.null(result$distrito))
+  expect_true(!is.null(result$provincia))
+  expect_true(!is.null(result$municipio))
+  expect_true(!is.null(result$ref.catastral))
+  expect_true(!is.null(result$url.ref.catastral))
+  expect_true(!is.null(result$tipo))
+  expect_true(!is.null(result$tipo.via))
+  expect_true(!is.null(result$nombre.via))
+  expect_true(!is.null(result$num.via))
+  expect_true(!is.null(result$num.via.id))
+  expect_true(!is.null(result$cod.postal))
 })
 
 test_that("cartociudad_reverse_geocode returns an address for a valid location", {
   result <- cartociudad_reverse_geocode(40.473219, -3.7227241)
 
-  expect_that(!is.null(result$tipo),       is_true())
-  expect_that(!is.null(result$tipo.via),   is_true())
-  expect_that(!is.null(result$nombre.via), is_true())
-  expect_that(!is.null(result$num.via),    is_true())
-  expect_that(!is.null(result$num.via.id), is_true())
-  expect_that(!is.null(result$municipio),  is_true())
-  expect_that(!is.null(result$provincia),  is_true())
-  expect_that(!is.null(result$cod.postal), is_true())
+  expect_true(!is.null(result$tipo))
+  expect_true(!is.null(result$tipo.via))
+  expect_true(!is.null(result$nombre.via))
+  expect_true(!is.null(result$num.via))
+  expect_true(!is.null(result$num.via.id))
+  expect_true(!is.null(result$municipio))
+  expect_true(!is.null(result$provincia))
+  expect_true(!is.null(result$cod.postal))
 })
 
 test_that("get_cartociudad_user_agent returns the package name and github repo url", {
@@ -49,12 +47,12 @@ test_that("get_cartociudad_user_agent returns the package name and github repo u
   httr::stop_for_status(result)
 
   user.agent <- httr::content(result)$"user-agent"
-  expect_that(length(grep("caRtociudad/[0-9.]+",               user.agent)) == 1, is_true())
-  expect_that(length(grep("github.com/rOpenSpain/caRtociudad", user.agent)) == 1, is_true())
+  expect_true(length(grep("caRtociudad/[0-9.]+", user.agent)) == 1)
+  expect_true(length(grep("github.com/rOpenSpain/caRtociudad", user.agent)) == 1)
 })
 
 test_that("cartociudad_get_area with valid parameters returns a polygon", {
   result <- cartociudad_get_area(40.3930144, -3.6596683, 500)
 
-  expect_that(nrow(result) > 2, is_true())
+  expect_true(nrow(result) > 2)
 })
